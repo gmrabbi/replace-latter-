86f866761230bb8c08e7950705f416622b477782
Golam Mostafa Rabbi <gmrabbi91221@gmail.com>
2021-02-03 16:59:16 +0600

    Now it work properly.

--- replace_latter.py
+++ replace_latter.py
@@ -5,6 +5,36 @@ root = Tk()
 root.title("Replace latter")
 
 
+def result():
+    user_input = (phase_entry.get()).lower()
+    start_latter = ((str_ltr.get()).lower())[0]
+
+    alphabet = []
+    for i in range(97, 122 + 1):
+        alphabet += chr(i)
+
+    user_alphabet = []
+    for i in range((ord(start_latter)), 122 + 1):
+        user_alphabet += chr(i)
+
+    firt_latter = user_alphabet[0]
+
+    if len(user_alphabet) < 26:
+        for i in range(97, ord(firt_latter)):
+            user_alphabet += chr(i)
+
+    latter_num = alphabet.index(user_input[0])
+
+    output = ""
+    for latter in user_input:
+        if latter in alphabet:
+            num = alphabet.index(latter)
+            output += (user_alphabet[num - latter_num]).upper()
+        else:
+            output += latter
+    messagebox.showinfo("Result", output)
+
+
 lbl1 = Label(root, font=("arial", 15, "bold"), text="Phase : ")
 phase_entry = Entry(root, font=("arial", 15, "bold"), bd=5)
 lbl1.grid(row=0, column=0)
@@ -16,38 +46,7 @@ lbl_ltr.grid(row=1, column=0)
 str_ltr.grid(row=1, column=1)
 
 btn = Button(root, font=("arial", 18, "bold"),
-             text="Submit", bd=5)
+             text="Submit", bd=5, command=result)
 btn.grid(row=2, column=1)
 
 mainloop()
-
-# user_input = input("Enter your phase : ").lower()
-# start_latter = (input("Enter the starting latter : ").lower())[0]
-
-# alphabet = []
-
-# for i in range(97, 122 + 1):
-#     alphabet += chr(i)
-
-
-# user_alphabet = []
-# for i in range((ord(start_latter)), 122 + 1):
-#     user_alphabet += chr(i)
-
-# firt_latter = user_alphabet[0]
-# if len(user_alphabet) < 26:
-#     for i in range(97, ord(firt_latter)):
-#         user_alphabet += chr(i)
-
-
-# latter_num = alphabet.index(user_input[0])
-
-# output = ""
-# for latter in user_input:
-#     if latter in alphabet:
-#         num = alphabet.index(latter)
-#         output += (user_alphabet[num - latter_num]).upper()
-#     else:
-#         output += latter
-
-# print(output)
